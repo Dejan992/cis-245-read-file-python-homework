2494118db1b18014d82979e72bfb9d9a0a1eee0c
Dejan Rajkovic <37712877+Dejan992@users.noreply.github.com>
2022-02-14 16:59:46 -0700

    school work

---
+++ file-read.py
@@ -0,0 +1,23 @@
+import os
+
+def main():
+    directory_input = input("Please provide the directory where you would like to store the file: ")
+    file_name_input = input("Please provide the name of the file you wish to create: ")
+    text_file = f"{file_name_input}.txt"
+    full_path = os.path.join(directory_input, text_file)
+
+    if not os.path.isdir(directory_input):
+        os.makedirs(directory_input)
+
+    name_input = input("Please provide your full name: ")
+    address_input = input("Please provide your address: ")
+    phone_number_input = input("Please provide your phone number: ")
+
+    with open(full_path, 'w') as file:
+        file.write(f'Full Name: {name_input}, \nAdress: {address_input}, \nPhone Number: {phone_number_input}')
+
+    with open(full_path, 'r') as file:
+        file_read = file.read()
+        print(file_read)
+if __name__=="__main__":
+    main()
